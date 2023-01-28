8c0c7bdcbdb3110a4c7f1069a318427daa963b2d
José Julio Umbelino <josejulioumbelino.dev@gmail.com>
2023-01-27 22:46:50 -0300

    api

---
+++ api-login-autenticacao/src/main/java/br/com/model/User.java
@@ -0,0 +1,31 @@
+package br.com.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity(name = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	@Column(length = 150, nullable = false, unique=true)
+	private String email;
+	
+	@Column(length = 30, nullable = false)
+	private String password;
+}
