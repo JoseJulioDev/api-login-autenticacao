6b01daacb56655e804e3b5e102b242b9bfa303a9
José Julio Umbelino <josejulioumbelino.dev@gmail.com>
2023-01-29 14:09:58 -0300

    autenticando o usuário com os métodos do spring security

--- api-login-autenticacao/src/main/java/br/com/controller/UserController.java
+++ api-login-autenticacao/src/main/java/br/com/controller/UserController.java
@@ -3,6 +3,10 @@ package br.com.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AuthenticationManager authManager;
+	
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody User user) {
 		User createdUser = userService.save(user);
@@ -28,9 +35,13 @@ public class UserController {
 	
 	@PostMapping("/login")
 	public ResponseEntity<User> login(@RequestBody UserLogindto user) {
-		User loggedUser = userService.login(user.getEmail(), user.getPassword());
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
 		
-		return ResponseEntity.ok(loggedUser);
+		Authentication auth = authManager.authenticate(token);
+		
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		
+		return ResponseEntity.ok(null);
 	}
 	
 }
