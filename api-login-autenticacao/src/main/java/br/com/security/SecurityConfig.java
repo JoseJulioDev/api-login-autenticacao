e1ba8a7c8453b4c23f3253d917a08d822333fcf0
José Julio Umbelino <josejulioumbelino.dev@gmail.com>
2023-01-30 21:54:01 -0300

    Adicionando proteção a cada requisição so acessar o conteúdo caso estaja autenticado

--- api-login-autenticacao/src/main/java/br/com/security/SecurityConfig.java
+++ api-login-autenticacao/src/main/java/br/com/security/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.com.service.UserService;
 
@@ -33,6 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests()
+		.anyRequest().authenticated();
+		
+		http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
